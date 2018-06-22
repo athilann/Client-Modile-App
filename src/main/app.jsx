06f72e3563d87bb5ba79aeb7f0ebbee377ee0429
Athila Naniny Ninow <athila@catamoeda.com.br>
2018-06-22 14:52:21 -0300

    ajusting the config

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -1,8 +1,7 @@
 import React from "react";
-//import LoginPage from "material-kit-react/src/views/LoginPage/LoginPage";
 
 export default props => (
   <div className="wrapper">
-      <h1>App</h1>
+      <h1>App</h1>
   </div>
 );
--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,12 @@ const ExtractTextPlugin = require('mini-css-extract-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
-module.exports = { 
-
+module.exports = {
     mode: modoDev ? 'development' : 'production',
     entry: './src/index.jsx',
-
-    output: { 
+    output: {
         path: __dirname + '/public',
-        filename: './app.js' 
+        filename: './app.js'
     },
     optimization: {
         minimizer: [
@@ -22,46 +20,43 @@ module.exports = { 
             new OptimizeCssAssetsPlugin({})
         ]
     },
-    devServer: { 
+    devServer: {
         port: 8080,
         contentBase: './public',
-         
+
     },
-    resolve: { 
+    resolve: {
         extensions: ['*', '.js', '.jsx'],
-        alias: { 
+        alias: {
             modules: __dirname + '/node_modules',
             jquery: 'modules/admin-lte/plugins/jQuery/jquery.min.js',
-            bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js' 
-        } 
+            bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js'
+        }
     },
-    plugins: [ new webpack.ProvidePlugin({ 
+    plugins: [new webpack.ProvidePlugin({
         $: 'jquery',
         jQuery: 'jquery',
-        'window.jQuery': 'jquery' 
-    }),  new ExtractTextPlugin({
+        'window.jQuery': 'jquery'
+    }), new ExtractTextPlugin({
         filename: 'app.css'
-    }) ],
-     module: { 
-        rules: [{ 
+    })],
+    module: {
+        rules: [{
             test: /.js[x]?$/,
-            loader : 'babel-loader',
             exclude: /node_modules/,
-            query: { 
-                babelrc: false,
-                presets: ['@babel/preset-env','@babel/preset-react','stage-0'],
-                plugins: ['@babel/plugin-proposal-object-rest-spread'] 
-            } 
-        }, { 
+            use: {
+                loader: "babel-loader"
+            }
+        }, {
             test: /\.s?[as]ss$/,
             use: [
                 ExtractTextPlugin.loader,
                 'css-loader',
                 'sass-loader'
             ]
-        }, { 
+        }, {
             test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
-            use : ['file-loader'] 
+            use: ['file-loader']
         }],
     }
 };
